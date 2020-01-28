cff0581dbdb41b417db26b54be7043fe5805810e
Borys Pachocki <borys6ff@gmail.com>
2020-01-28 14:02:41 +0100

    some structure reorganization, added calculation of entropy mean and plotting from .csv file

--- execute/execute_byte_conversion.py
+++ execute/execute_byte_conversion.py
@@ -2,7 +2,7 @@ import os
 
 from prepare_data import byte_data
 
-folder = '10.01.2020_19.35_25'
+folder = '17.01.2020_20.16'
 processed_up_path = 'V:\\thesis\\processed_data'
 processed_path = os.path.join(processed_up_path, folder)
 input_files = os.listdir(processed_path)
